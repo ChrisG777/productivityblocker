b3146a82301d58e16ceaa292c314c4286346b130
Chris Ge <chrisge@MacBook-Pro-30.local>
2022-07-24 16:54:59 -0400

    The alarm now is only set when youtube.com is switched to. When the alarm goes off, it only gives off an alert on the tab that's already active and doesn't switch to the unproductive tab.

--- eventPage.js
+++ eventPage.js
@@ -1,21 +1,16 @@
+const blockedSites = ["youtube.com"];
+
+function timer()
+{
+  alert("Timer has been set");
+}
 function blocker()
 {
   alert("Page has been blocked!");
 }
-
-var link;
-
-// function sendCurrentUrl() {
-//   chrome.tabs.query(
-//       {active: true, currentWindow: true},
-//       (tabs) => {
-//         var tablink = tabs[0].url;
-//         chrome.storage.sync.set({ link: tablink});
-//         console.log(tablink);
-//       });
-// }
-
-
+/* 
+listener for alarms
+*/
 chrome.alarms.onAlarm.addListener(function(alarm) {
   chrome.tabs.query(
       {active: true, currentWindow: true},
@@ -29,6 +24,37 @@ chrome.alarms.onAlarm.addListener(function(alarm) {
       });
 });
 
+/*
+creating an alarm
+*/
+var alarmClock = {
+        setup: function(urlName) {
+             chrome.alarms.create(urlName, {delayInMinutes: 1} );
+        }
+};
+
+function doToggleAlarm(curUrl) {
+    alarmClock.setup(curUrl);
+    chrome.alarms.getAll(function(alarms) {
+       var hasAlarm = alarms.some(function(a) {
+          chrome.tabs.query(
+          {active: true, currentWindow: true},
+          (tabs) => {
+              chrome.scripting.executeScript(
+                  {
+                      target: { tabId: tabs[0].id},
+                      func: timer,
+                  }
+              );
+          });
+       });
+    });
+}
+
+
+/*
+listeners for tab changes
+*/
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.url) run(tab);
 });
@@ -39,6 +65,17 @@ chrome.tabs.onActivated.addListener(info => {
 
 const processingTabId = {};
 
+function checkUrl(curUrl) {
+  for (i of blockedSites)
+  {
+    if (curUrl.includes(i))
+    {
+      return true;
+    }
+  }
+  return false; 
+}
+
 function run(tab) {
   if (processingTabId[tab.id]) return;
   processingTabId[tab.id] = true;
@@ -47,13 +84,11 @@ function run(tab) {
     currentHost = newUrl.host;
     currentUrl = tab.url;
     console.log(currentUrl);
+    if (checkUrl(currentUrl))
+    {
+      doToggleAlarm(currentUrl);
+    }
   }
-  
-  // doSomeAsyncStuff(() => {
-  //   //................................
-  //   // when all done:
-  //   delete processingTabId[tab.id];
-  // });
 }
 
 
