6c79099dd57bd447fe48e9e9bff02c08e77260c4
Chris Ge <chrisge@MacBook-Pro-30.local>
2022-08-07 13:48:08 -0700

    Made the extension popup just be a button that leads to the options page

---
+++ popup.js
@@ -0,0 +1,8 @@
+//copied from the chrome documentation
+document.querySelector('#optionsbutton').addEventListener('click', function() {
+  if (chrome.runtime.openOptionsPage) {
+    chrome.runtime.openOptionsPage();
+  } else {
+    window.open(chrome.runtime.getURL('options.html'));
+  }
+});
